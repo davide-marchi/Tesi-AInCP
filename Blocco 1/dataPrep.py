c56cb58a0af80752e102788842076c76b5a703d5
giordanoscerra <121350206+giordanoscerra@users.noreply.github.com>
2022-12-28 19:22:37 +0100

    Update dataPrep.py

--- Blocco 1/dataPrep.py
+++ Blocco 1/dataPrep.py
@@ -3,51 +3,53 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from sklearn.model_selection import train_test_split
-
+from sktime.datasets import load_arrow_head
 from sktime.clustering.k_means import TimeSeriesKMeans
 from sktime.clustering.k_medoids import TimeSeriesKMedoids
 from sktime.clustering.utils.plotting._plot_partitions import plot_cluster_algorithm
 
 
 # Creating dataframe
-folder = 'C:/Users/david/Documents/University/Tesi/Python AInCP/'
-df = pd.read_csv(folder + 'only AC/data/9_AHA_1sec.csv')
+folder = 'C:/Users/giord/Downloads/only AC data/only AC/data/'
+#folder = 'C:/Users/david/Documents/University/Tesi/Python AInCP/'
+series = []
+for i in range (1,61):
+    df = pd.read_csv(folder + str(i) + '_AHA_1sec.csv')
+    # Calculating magnitude
+    magnitude_D = np.sqrt(np.square(df['x_D']) + np.square(df['y_D']) + np.square(df['z_D']))
+    magnitude_ND = np.sqrt(np.square(df['x_ND']) + np.square(df['y_ND']) + np.square(df['z_ND']))
+    # Concat
+    series.append(pd.concat([magnitude_D, magnitude_ND], ignore_index = True))
 
-# Calculating magnitude
-magnitude_D = np.sqrt(np.square(df['x_D']) + np.square(df['y_D']) + np.square(df['z_D']))
-magnitude_ND = np.sqrt(np.square(df['x_ND']) + np.square(df['y_ND']) + np.square(df['z_ND']))
-
-# Concat
-mag_concat = pd.concat([magnitude_D, magnitude_ND], ignore_index = True)
-
-print(mag_concat.shape)
-print(type(mag_concat))
-print(mag_concat)
-
-
-# Create some Series objects
-s1 = pd.Series([1, 2, 3])
-s2 = pd.Series([4, 5, 6])
-s3 = pd.Series([7, 8, 9])
 
 # Create a DataFrame with a single column of Series objects
-X = pd.DataFrame({'column': [s1, s2, s3]})
+X = pd.DataFrame({'series': series})
 print(X.shape)
 print(type(X))
 print(X)
 
 k_means = TimeSeriesKMeans(
-    n_clusters=5,  # Number of desired centers
-    init_algorithm="forgy",  # Center initialisation technique
+    n_clusters=2,  # Number of desired centers
+    init_algorithm="random",  # Center initialisation technique
     max_iter=10,  # Maximum number of iterations for refinement on training set
     metric="dtw",  # Distance metric to use
     averaging_method="mean",  # Averaging technique to use
     random_state=1,
 )
 
+k_means.set_tags(**{"X_inner_mtype": "numpy3D",
+"multivariate": False,
+        "unequal_length": True,
+        "missing_values": False,
+        "multithreading": False})
+
 k_means.fit(X)
 plot_cluster_algorithm(k_means, X, k_means.n_clusters)
-
-# Polotting the concatted data
-plt.plot(mag_concat)
+'''''
+fig, axs = plt.subplot(235)
+for x in series:
+    i = 0
+    axs[0] = plt.plot(x)
+    i += 1
 plt.show()
+'''''
