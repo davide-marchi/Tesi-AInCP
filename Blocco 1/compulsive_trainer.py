f188d992be47bf4519efb1b55c9aabe9e78f944f
giordanoscerra <121350206+giordanoscerra@users.noreply.github.com>
2023-01-12 15:43:24 +0100

    ..

--- Blocco 1/compulsive_trainer.py
+++ Blocco 1/compulsive_trainer.py
@@ -3,15 +3,13 @@ from train_kmeans_model import train_kmeans_model
 folder = 'C:/Users/giord/Downloads/only AC data/only AC/'
 #folder = 'C:/Users/david/Documents/University/Tesi/Python AInCP/only AC/'
 
-#FIRST CRASH: manca k4_w600_dtw_dba, k4_w600_dtw_mean, k4_w600_euclidean_dba, k4_w600_euclidean_mean
-#             manca k4_w900_dtw_dba, k4_w900_dtw_mean, k4_w900_euclidean_dba, k4_w900_euclidean_mean
 
 l_window_size=[300,600,900] # 3 values
-l_n_clusters=[2,3,4] # 1 values
+l_n_clusters=[2,3,4,5] # 1 values
 l_init_algorithm=['kmeans++'] # 1 values
 l_max_iter=[30]
-l_metric=['euclidean'] # 1 values
-l_averaging_method=['mean'] # 1 values
+l_metric=['dtw'] # 1 values
+l_averaging_method=['dba'] # 1 values
 patients = 34
 
 models_number = len(l_window_size) * len(l_n_clusters) * len(l_init_algorithm) * len(l_max_iter) * len(l_metric) * len(l_averaging_method)
