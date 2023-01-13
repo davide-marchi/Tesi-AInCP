e4d5c1d242512269efa7e6cca6a3d6ff8904043f
giordanoscerra <121350206+giordanoscerra@users.noreply.github.com>
2023-01-13 18:45:10 +0100

    test sul week

--- Blocco 1/cluster_week_samples.py
+++ Blocco 1/cluster_week_samples.py
@@ -2,12 +2,16 @@ import pandas as pd
 import re
 import joblib as jl
 import os
+import numpy as np
 
 
 ############
-folder = 'C:/Users/david/Documents/University/Tesi/Python AInCP/only AC/'
+#folder = 'C:/Users/david/Documents/University/Tesi/Python AInCP/only AC/'
+folder = 'C:/Users/giord/Downloads/only AC data/only AC/'
 model_folder = 'Blocco 1/60_patients/KMeans/KMEANS_K2_W900_I30_kmeans++_euclidean_mean/'
 ############
+
+metadata = pd.read_excel(folder + 'metadata2022_04.xlsx')
 
 # Define the samples size
 match = re.search(r"_W(\d+)", model_folder)
@@ -21,16 +25,47 @@ else:
 metadata = pd.read_excel(folder + 'metadata2022_04.xlsx')
 
 guessed_patients = 0
+indecisi = 0
 
 for i in range (1,61):
 
     df = pd.read_csv(folder + 'data/' + str(i) + '_week_1sec.csv', chunksize=sample_size)
-    week_lenght = df.shape[0]
+    #week_lenght = df.shape[0]  
 
-    cluster_0_samples = 0
-    cluster_1_samples = 0
-
-    print(df)
+    cluster_0_samples = 0 #malati
+    cluster_1_samples = 0 #sani
+    series=[]
+    
+    for chunk in df:
+        magnitude_D = np.sqrt(np.square(chunk['x_D']) + np.square(chunk['y_D']) + np.square(chunk['z_D']))
+        magnitude_ND = np.sqrt(np.square(chunk['x_ND']) + np.square(chunk['y_ND']) + np.square(chunk['z_ND']))
+        magnitude_concat = pd.concat([magnitude_D, magnitude_ND], ignore_index = True)
+        series.append(magnitude_concat)
+        
+    X = pd.DataFrame({'series': series}) 
+    prediction = model.predict(X)
+    for n in prediction:
+        if n == 0:
+            cluster_0_samples+=1
+        else:
+            cluster_1_samples+=1    
+    
+    if cluster_0_samples > cluster_1_samples and metadata['hemi'].iloc[i-1] == 2:
+        print("ho indovinato un paziente malato. bene !")
+        guessed_patients+=1
+    elif cluster_0_samples > cluster_1_samples and metadata['hemi'].iloc[i-1] == 1:
+        print("ho sbagliato un paziente sano. pensavo fosse malato !")
+    elif cluster_0_samples < cluster_1_samples and metadata['hemi'].iloc[i-1] == 1:
+        print("ho indovinato un paziente sano. bene !")
+        guessed_patients+=1
+    elif cluster_0_samples < cluster_1_samples and metadata['hemi'].iloc[i-1] == 2:
+        print("ho sbagliato un paziente malato. pensavo fosse sano !")
+    elif cluster_0_samples == cluster_1_samples:
+        print("eh qui non so decidermi molto bene.")
+        indecisi+=1
+        
+print("guessed patients: " , guessed_patients, "/60 (", (guessed_patients/60)*100, "%)")
+print("indecisi: " , indecisi, "/60 (", (indecisi/60)*100, "%)")
 
     # Iterate through the DataFrame
     #for j, rows in enumerate(df.iterrows()):
