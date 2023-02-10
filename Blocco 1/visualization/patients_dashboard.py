d1de50331ac5b710eef1078151fc3430a8e822c1
Davide <davidemarchi00@gmail.com>
2023-02-10 03:11:59 +0100

    Pulito un po' il patients dashbord spostando codice e stampe

--- Blocco 1/visualization/patients_dashboard.py
+++ Blocco 1/visualization/patients_dashboard.py
@@ -1,26 +1,16 @@
-import pandas as pd
+# Importazione di librerie e funzioni
 import re
-import joblib as jl
 import os
-import numpy as np
-import matplotlib.pyplot as plt
-from elaborate_magnitude import elaborate_magnitude
 import datetime
 import matplotlib
+import numpy as np
+import pandas as pd
+import joblib as jl
+import matplotlib.pyplot as plt
+from elaborate_magnitude import elaborate_magnitude
 
 
-
-############
-#folder = 'C:/Users/david/Documents/University/Tesi/Python AInCP/only AC/'
-folder = 'C:/Users/giord/Downloads/only AC data/only AC/'
-
-model_name = 'KMEANS_K2_W600_kmeans++_euclidean_mean'
-
-operation_type = 'concat'
-
-model_folder = 'Blocco 1/'+ operation_type +'_version/60_patients/KMeans/' + model_name + '/'
-############
-    
+# Funzione per prendere la media del primo quartile dei massimi valori di una lista
 def first_quartile_of_max(my_list):
     sorted_list = sorted(my_list, reverse=True)
     quartile_length = len(sorted_list) // 2
@@ -29,67 +19,32 @@ def first_quartile_of_max(my_list):
     first_quartile = sum(sorted_list[:quartile_length]) / quartile_length
     return first_quartile
 
-def plot_daily_trend():
-    ################################################### DAILY TREND
-    sub_Y = np.array_split(Y, 6)
-    fig1, axs1 = plt.subplots(2,3)
-    fig1.suptitle('daily trend')
-    subplots_day_D = [magnitude_D[n:n+int(len(magnitude_D)/6)] for n in range(0, len(magnitude_D), int(len(magnitude_D)/6))]
-    subplots_day_ND = [magnitude_ND[n:n+int(len(magnitude_D)/6)] for n in range(0, len(magnitude_ND),int(len(magnitude_D)/6))]
-    kek=-3
-    for z in range(0,2):
-        kek+=3
-        for a in range(0,3):
-            sub_Y_multiplied=[]
-            for el in sub_Y[a+kek]:
-                for u in range(sample_size):
-                    sub_Y_multiplied.append(el)
-        
-            #axs1[z, a].scatter(list(range(len(subplots_day_D[a+kek]))), subplots_day_D[a+kek], c=sub_Y_multiplied, cmap= 'brg', s=5, alpha=0.5)
-            #axs1[z, a].scatter(list(range(len(subplots_day_ND[a+kek]))), subplots_day_D[a+kek], c=sub_Y_multiplied, cmap= 'brg',s=5, alpha=0.5)          
-            
-            subplotD = [subplots_day_D[a+kek].iloc[n:n+sample_size] for n in range(0, len(subplots_day_D[a+kek]), sample_size)]
-            subplotND = [subplots_day_ND[a+kek].iloc[n:n+sample_size] for n in range(0, len(subplots_day_ND[a+kek]), sample_size)]
-            for s in range(len(subplotD)):
-                if sub_Y[a+kek][s] == 0:
-                    axs1[z, a].plot(subplotD[s], color='darkred', alpha=0.5)
-                    axs1[z, a].plot(subplotND[s], color='red', alpha=0.5)
-                elif sub_Y[a+kek][s] == 1:
-                    axs1[z, a].plot(subplotD[s], color='darkgreen', alpha=0.5)
-                    axs1[z, a].plot(subplotND[s], color='green', alpha=0.5)
-                else:
-                    axs1[z, a].plot(subplotD[s], color='darkblue', alpha=0.5)
-                    axs1[z, a].plot(subplotND[s], color='blue', alpha=0.5)
 
-    plt.show()
-    plt.close()
-    ################################################### DAILY TREND  
+############
+folder = 'C:/Users/david/Documents/University/Tesi/Python AInCP/only AC/'
+#folder = 'C:/Users/giord/Downloads/only AC data/only AC/'
+model_name = 'KMEANS_K2_W600_kmeans++_euclidean_mean'
+operation_type = 'concat'
+model_folder = 'Blocco 1/'+ operation_type +'_version/60_patients/KMeans/' + model_name + '/'
+############
 
-
-
-# Define the samples size
+  
+# Lettura della dimensione della finestra, ricerca del modello e caricamento
 match = re.search(r"_W(\d+)", model_name)
 
 if not(match) or not(os.path.exists(model_folder + "trained_model")):
     print("Model not found or invalid sample size")
-    exit(1)  
+    exit(1)
 
-model = jl.load(model_folder + "trained_model")
 sample_size = int(match.group(1))
-
-metadata = pd.read_excel(folder + 'metadata2022_04.xlsx')
-metadata.drop(['age_aha', 'gender', 'dom', 'date AHA', 'start AHA', 'stop AHA'], axis=1, inplace=True)
-
-stats = pd.read_csv(model_folder + 'statistiche.csv')
-hemi_cluster = int(float(stats['AHA'][2]) > float(stats['AHA'][3]))
+model = jl.load(model_folder + "trained_model")
 
 if (not os.path.exists('Blocco 1/visualization/regressor_AHA2HP_' + model_name)) or (not os.path.exists('Blocco 1/visualization/regressor_HP2AHA_' + model_name)) :
-    print('a linear regressor was not found')
+    print('A linear regressor was not found')
     exit(1)
 
 lin_reg_AHA2HP = jl.load('Blocco 1/visualization/regressor_AHA2HP_' + model_name)
 lin_reg_HP2AHA = jl.load('Blocco 1/visualization/regressor_HP2AHA_' + model_name)
-
 
 if not os.path.exists('Blocco 1/visualization/timestamps_list'):
     start = datetime.datetime(2023, 1, 1)
@@ -104,27 +59,27 @@ if not os.path.exists('Blocco 1/visualization/timestamps_list'):
 else:
     timestamps = jl.load('Blocco 1/visualization/timestamps_list')
 
+metadata = pd.read_excel(folder + 'metadata2022_04.xlsx')
+metadata.drop(['age_aha', 'gender', 'dom', 'date AHA', 'start AHA', 'stop AHA'], axis=1, inplace=True)
+
+stats = pd.read_csv(model_folder + 'statistiche.csv')
+hemi_cluster = int(float(stats['AHA'][2]) > float(stats['AHA'][3]))
 
 guessed = []
 healthy_percentage = []
 max_hp_list = []
-max_quart_hp_list= []
+max_quart_hp_list = []
 
 for i in range (1,61):
 
-    aha = metadata['AHA'].iloc[i-1]
-    cluster_hemiplegic_samples = 0 #malati
-    cluster_healthy_samples = 0 #sani
-    series = []
-    to_discard = []
-
     df = pd.read_csv(folder + 'data/' + str(i) + '_week_1sec.csv')
-    
-    #print("Inizio fase chunking")
-
     magnitude_D = np.sqrt(np.square(df['x_D']) + np.square(df['y_D']) + np.square(df['z_D']))
     magnitude_ND = np.sqrt(np.square(df['x_ND']) + np.square(df['y_ND']) + np.square(df['z_ND']))
 
+    series = []
+    to_discard = []
+
+    # Fase di chunking
     for j in range (0, len(magnitude_D), sample_size):
 
         chunk_D = magnitude_D.iloc[j:j + sample_size]
@@ -137,17 +92,16 @@ for i in range (1,61):
             if chunk_D.agg('sum') == 0 and chunk_ND.agg('sum') == 0:
                 to_discard.append(int(j/sample_size))
 
-
-    #print("Inizio fase predizione")
+    # Fase di predizione
     Y = model.predict(np.array(series))
 
     for index in to_discard:
         Y[index] = -1
 
+    cluster_healthy_samples = 0     # Non emiplegici
+    cluster_hemiplegic_samples = 0  # Emiplegici
 
-    #print("Inizio fase incrementi e stampe")
     for k in range(len(Y)):
-        # Presupponendo che i pazienti emiplegici siano nel cluster 0
         if Y[k] == hemi_cluster:
             cluster_hemiplegic_samples += 1
             Y[k] = -1
@@ -157,6 +111,29 @@ for i in range (1,61):
         else:
             Y[k] = 0
 
+    aha = metadata['AHA'].iloc[i-1]
+    is_hemiplegic = (metadata['hemi'].iloc[i-1] == 2)
+    hp_tot = np.nan
+    aha_from_hp_tot = np.nan
+    guess = not((cluster_hemiplegic_samples > cluster_healthy_samples) ^ is_hemiplegic) if cluster_hemiplegic_samples!=cluster_healthy_samples else 'uncertain'
+    guessed.append(guess)
+    
+    if (cluster_healthy_samples != 0 or cluster_hemiplegic_samples != 0):
+        hp_tot = (cluster_healthy_samples / (cluster_hemiplegic_samples + cluster_healthy_samples)) * 100
+        aha_from_hp_tot = (lin_reg_HP2AHA.predict([[hp_tot]]))[0] 
+        aha_from_hp_tot = 100 if aha_from_hp_tot > 100 else aha_from_hp_tot
+
+    healthy_percentage.append(hp_tot)
+    predicted_h_perc = lin_reg_AHA2HP.predict([[aha]])
+
+    print('Patient ', i)
+    print(' - Guessed: ', guess)
+    print(' - AHA:     ', aha)
+    print(' - HP:      ', round(hp_tot, 2))
+    print(' - AHA predicted from HP: ', round(aha_from_hp_tot, 2))
+    print(' - HP predicted from AHA: ', round(predicted_h_perc[0], 2), '\n')
+
+    # Fase di plotting
     fig, axs = plt.subplots(7)
     fig.suptitle('Patient ' + str(i) + ' week trend, AHA: ' + str(aha))
     
@@ -172,7 +149,7 @@ for i in range (1,61):
     trend_block_size = 72            # Numero di finestre (da 600 secondi) raggruppate in un blocco
     significativity_threshold = 50   # Percentuale di finestre in un blocco che devono essere prese per renderlo significativo
 
-    ############# ANDAMENTO A BLOCCHI #############
+    #################### ANDAMENTO A BLOCCHI ####################
     h_perc_list = []
     subList = [Y[n:n+trend_block_size] for n in range(0, len(Y), trend_block_size)]
     for l in subList:
@@ -187,9 +164,9 @@ for i in range (1,61):
     axs[2].grid()
     axs[2].set_ylim([-1,101])
     axs[2].plot(h_perc_list, drawstyle = 'steps-post')
-    #####################################################
+    #############################################################
     
-    ###################### AI PLOT ###############################
+    ########################## AI PLOT ##########################
     ai_list = []
     subList_magD = [magnitude_D[n:n+(trend_block_size*sample_size)] for n in range(0, len(magnitude_D), trend_block_size*sample_size)]
     subList_magND = [magnitude_ND[n:n+(trend_block_size*sample_size)] for n in range(0, len(magnitude_ND), trend_block_size*sample_size)]
@@ -202,10 +179,9 @@ for i in range (1,61):
     axs[3].grid()
     axs[3].set_ylim([-101,101])
     axs[3].plot(ai_list)
-    ####################################################
+    #############################################################
     
-    
-    ############# ANDAMENTO SMOOTH #############
+    ##################### ANDAMENTO SMOOTH ######################
     aha_list_smooth = []
     h_perc_list_smooth = []
     h_perc_list_smooth_significativity = []
@@ -217,14 +193,12 @@ for i in range (1,61):
         if (((n_hemi + n_healthy) / trend_block_size) * 100) < significativity_threshold:
             h_perc_list_smooth.append(np.nan)
             aha_list_smooth.append(np.nan)
-
         else:
             h_perc_list_smooth.append((n_healthy / (n_hemi + n_healthy)) * 100)
             predicted_aha = (lin_reg_HP2AHA.predict([[h_perc_list_smooth[-1]]]))[0]
             aha_list_smooth.append(predicted_aha if predicted_aha <= 100 else 100) 
         #print("h_perc: ",h_perc_list_smooth[-1]," aha_pred: ",aha_list_smooth[-1], " diff: ",aha_list_smooth[-1]-h_perc_list_smooth[-1])
 
-    predicted_h_perc = lin_reg_AHA2HP.predict([[aha]])
     conf = 10
 
     filtered_list = list(filter(lambda x: not np.isnan(x), h_perc_list_smooth))
@@ -239,15 +213,14 @@ for i in range (1,61):
     axs[4].plot([np.nan if predicted_h_perc - conf <= x <= predicted_h_perc + conf else x for x in h_perc_list_smooth], c = 'red')
     axs[4].legend()
 
-
     axs[5].grid()
     axs[5].set_ylim([-1,101])
     axs[5].axhline(y = significativity_threshold, color = 'r', linestyle = '-', label='threshold')
     axs[5].plot(h_perc_list_smooth_significativity)
     axs[5].legend()
-    #####################################
+    #############################################################
 
-    ########### PREDICTED AHA PLOT ###############
+    ##################### PREDICTED AHA PLOT ####################
     #new_list = [[i] for i in h_perc_list_smooth]
     axs[6].grid()
     axs[6].set_ylim([-1,101])
@@ -255,37 +228,10 @@ for i in range (1,61):
     axs[6].plot(aha_list_smooth, c = 'green')
     axs[6].plot([np.nan if aha - conf <= x <= aha + conf else x for x in aha_list_smooth], c ='red')
     axs[6].legend()
+    #############################################################
     
-    #####################################
-    
-    #####################################
-    
-    
-    
-    
-    
-
-    is_hemiplegic = (metadata['hemi'].iloc[i-1] == 2)
-    hp_tot = np.nan
-    aha_from_hp_tot = np.nan
-    guess =not((cluster_hemiplegic_samples > cluster_healthy_samples) ^ is_hemiplegic) if cluster_hemiplegic_samples!=cluster_healthy_samples else 'uncertain'
-    print('Patient ', i, ' guessed: ', guess)
-    guessed.append(guess)
-
-    if (cluster_healthy_samples != 0 or cluster_hemiplegic_samples != 0):
-        hp_tot = (cluster_healthy_samples / (cluster_hemiplegic_samples + cluster_healthy_samples)) * 100
-        aha_from_hp_tot = (lin_reg_HP2AHA.predict([[hp_tot]]))[0] 
-        aha_from_hp_tot = 100 if aha_from_hp_tot > 100 else aha_from_hp_tot
-        
-
-    healthy_percentage.append(hp_tot)
-    print('AHA predicted from total HP: ', round(aha_from_hp_tot,2) , ' AHA was: ', aha, "\n----")
-
     plt.show()
     plt.close()
-
-    #plot_daily_trend()
-
 
 metadata['guessed'] = guessed
 metadata['healthy_percentage'] = healthy_percentage
@@ -311,5 +257,6 @@ metadata.plot.scatter(x='healthy_percentage', y='AI_aha', c='MACS', colormap='vi
 #metadata.plot.scatter(x='AI_week', y='AHA', c='MACS', colormap='viridis').get_figure().savefig(folder_name + 'plot_AI_week_AHA.png')
 #metadata.plot.scatter(x='AI_aha', y='AHA', c='MACS', colormap='viridis').get_figure().savefig(folder_name + 'plot_AI_aha_AHA.png')
 
-print("\nCoefficiente di Pearson tra max e aha: ", (np.corrcoef(max_hp_list, metadata['AHA'].values))[0][1])
-print("\nCoefficiente di Pearson tra maxquart e aha: ", (np.corrcoef(max_quart_hp_list, metadata['AHA'].values))[0][1], '\n')
+print("Coefficiente di Pearson tra hp e aha:       ", (np.corrcoef(metadata['healthy_percentage'], metadata['AHA'].values))[0][1])
+print("Coefficiente di Pearson tra max e aha:      ", (np.corrcoef(max_hp_list, metadata['AHA'].values))[0][1])
+print("Coefficiente di Pearson tra maxquart e aha: ", (np.corrcoef(max_quart_hp_list, metadata['AHA'].values))[0][1])
