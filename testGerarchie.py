26e3ecbda44b73283c7469a3939aaa30e93d8765
Davide <davidemarchi00@gmail.com>
2023-02-11 21:46:02 +0100

    Aggiunto file per testare le gerarchie dei modelli

---
+++ testGerarchie.py
@@ -0,0 +1,54 @@
+from sktime.clustering.k_means import TimeSeriesKMeans
+from sktime.clustering.k_medoids import TimeSeriesKMedoids
+
+'''
+model = TimeSeriesKMeans()
+
+print(type(model))
+print(type(TimeSeriesKMeans()))
+print(type(model) == type(TimeSeriesKMeans()))   <-------------------
+print(model.get_params(), '\n')
+#print(model.get_class_tags(), '\n')
+#print(model.get_tags(), '\n')
+
+model1 = TimeSeriesKMedoids()
+print(model1.get_params(), '\n')
+'''
+
+################################################
+
+from sktime.classification.interval_based import TimeSeriesForestClassifier
+from sktime.datasets import load_unit_test
+from sklearn.base import BaseEstimator
+
+X_train, y_train = load_unit_test(split="train", return_X_y=True)
+X_test, y_test = load_unit_test(split="test", return_X_y=True)
+
+clf = TimeSeriesForestClassifier(n_estimators=5)
+clf.fit(X_train, y_train)
+print(clf.get_params())
+print(clf.is_fitted)
+y_pred = clf.predict(X_test)
+
+clf.save('kek_model_clf')
+
+clf_loaded_bad = TimeSeriesForestClassifier()
+print(clf_loaded_bad.get_params())
+print(clf_loaded_bad.is_fitted)
+clf_loaded_bad.load_from_path('kek_model_clf.zip')
+print(clf_loaded_bad.get_params())
+print(clf_loaded_bad.is_fitted)
+
+clf_loaded_good = TimeSeriesForestClassifier().load_from_path('kek_model_clf.zip')
+print(clf_loaded_good.get_params())
+print(clf_loaded_good.is_fitted)
+
+# Sarebbe stato fico ma non funziona...
+estimator_loaded_good = BaseEstimator().load_from_path('kek_model_clf.zip')
+print(estimator_loaded_good.get_params())
+print(estimator_loaded_good.is_fitted)
+
+
+print(y_pred)
+#print(clf_loaded_bad.predict(X_test))
+print(clf_loaded_good.predict(X_test))
