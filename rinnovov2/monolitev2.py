3753582b6ae779fbb746a24732527d1d810bf7e2
Davide <davidemarchi00@gmail.com>
2023-02-16 17:14:34 +0100

    Modificati alcuni iperparametri del patients dashboard (72->36 e 10->5) e proseguimento nel monolitev2

--- rinnovov2/monolitev2.py
+++ rinnovov2/monolitev2.py
@@ -1,5 +1,7 @@
 import json
 import os
+import hashlib
+from sktime.base import BaseEstimator
 from train_clustering_model import train_clustering_model
 from test_clustering_model import test_clustering_model
 from patients_dashboard import patients_dashboard
@@ -11,29 +13,30 @@ data_folder = 'C:/Users/david/Documents/University/Tesi/Python AInCP/only AC/'
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 models_files = []
-models_files.append("models.json")
+models_files.append("modelsv2.json")
 
 for file in models_files:
 
     # Load JSON data from file
-    with open("models.json") as f:
+    with open(file) as f:
         data = f.read()
     
     # Convert JSON data into Model instances
-    models = json.loads(data)
+    models_specs = json.loads(data)
 
-    for model in models:
+    for specs in models_specs:
 
-        model_folder = "Trained_models/" + model["method"] + "/" + model["window_size"] + "/" + model["patients"] + "_patients/" + model["class_type"].split(".")[-1] + "/"
-        model_name = model["name"]
-
-        if not(os.path.exists(model_folder + model_name)):
+        model_folder = "Trained_models/" + specs["method"] + "/" + str(specs["window_size"]) + "_seconds/" + str(specs["patients"]) + "_patients/" + specs["class_type"].split(".")[-1] + "/" + "model_" + hashlib.sha256(json.dumps(specs["params"], sort_keys=True).encode()).hexdigest()[:10] + "/"
+        
+        if not(os.path.exists(model_folder + "trained_model.zip")):
 
             print("Model not found -> Training started\n")
-            train_clustering_model(data_folder, False, model, model_name)
+            train_clustering_model(data_folder, specs, model_folder, False)
+
+        model = BaseEstimator().load_from_path(model_folder + 'trained_model.zip')
 
         print("Testing on AHA sessions:\n")
-        test_clustering_model(data_folder, model_name,  model["method"] , model_folder)
+        #test_clustering_model(data_folder, model_name,  model["method"] , model_folder)
 
         print("Testing on WEEK sessions:\n")
-        patients_dashboard(data_folder, model_name, model["method"], model_folder)
+        #patients_dashboard(data_folder, model_name, model["method"], model_folder)
--- rinnovov2/train_clustering_model.py
+++ rinnovov2/train_clustering_model.py
@@ -4,79 +4,36 @@ import joblib as jl
 from create_windows import create_windows
 from save_model_stats import save_model_stats
 
-from sktime.clustering.k_means import TimeSeriesKMeans
-from sktime.clustering.k_medoids import TimeSeriesKMedoids
-from sktime.clustering.k_shapes import TimeSeriesKShapes
-from sktime.clustering.kernel_k_means import TimeSeriesKernelKMeans
 from sktime.clustering.utils.plotting._plot_partitions import plot_cluster_algorithm
 
 import importlib
 
 
-def train_clustering_model(folder, plot_clusters, model, model_name):
-
-    '''
-    if (model['name'] == 'KMeans'):
-        AVERAGING_METHOD = 'mean' # defaults
-        if params['metric'] == 'dtw':
-            AVERAGING_METHOD = 'dba'
-        model_name='KMEANS_K'+str(params['n_clusters'])+'_W'+str(model['window_size'])+'_'+params['init_algorithm']+'_'+params['metric']+'_'+AVERAGING_METHOD
-        model = TimeSeriesKMeans()
-        model.set_params(**params)
-    elif(model['name'] == 'KMedoids'):
-        model_name='KMEDOIDS_K'+str(params['n_clusters'])+'_W'+str(model['window_size'])+'_'+params['init_algorithm']+'_'+params['metric']
-        model = TimeSeriesKMedoids()
-        model.set_params(**params)
-    elif(model['name'] == 'KShapes'):
-        model_name='KSHAPES_K'+str(params['n_clusters'])+'_W'+str(model['window_size'])
-        model = TimeSeriesKShapes()
-        model.set_params(**params)
-    elif(model['name'] == 'KernelKMeans'):
-        model_name='KERNELKMEANS_K'+str(params['n_clusters'])+'_W'+str(model['window_size'])
-        model = TimeSeriesKernelKMeans()
-        model.set_params(**params)
-    else:
-        print("Invalid model requested")
-        exit(1)
-    '''
-    class_string = model["class_type"]
+def train_clustering_model(data_folder, specs, model_folder, plot_clusters):
 
     # Split the string into the module and class names
-    module_name, class_name = class_string.rsplit(".", 1)
+    module_name, class_name = specs["class_type"].rsplit(".", 1)
+    model = getattr(importlib.import_module(module_name), class_name)()
 
-    # Import the module
-    module = importlib.import_module(module_name)
+    model.set_params(**specs['params'])
 
-    # Get the class object from the module
-    class_obj = getattr(module, class_name)
-
-    modello = class_obj()
-
-    modello.set_params(**model['params'])
-
-    series, y_AHA, y_MACS, total, taken, lost = create_windows(model['window_size'], folder,model['method'], model['patients'])
+    series, y_AHA, y_MACS, total, taken, lost = create_windows(specs['window_size'], data_folder, specs['method'], specs['patients'])
 
     # Create a DataFrame with a single column of Series objects
     X = np.array(series)
 
-    model_folder = "./Blocco 1/"+model['method']+"_version/" + str(model['patients']) + "_patients/" + model['name'] + "/" + model_name
+    y_pred = model.fit_predict(X)
+    os.makedirs(model_folder)
+    model.save(model_folder + "/trained_model")
 
-    if not os.path.exists(model_folder + "/trained_model"):
-        y_pred = modello.fit_predict(X)
-        os.makedirs(model_folder)
-        jl.dump(modello, model_folder + "/trained_model")
-        with open(model_folder + '/parametri.txt', 'w') as f:
-            f.write('Total = ' + str(total) + ' seconds\n')
-            f.write('Trained on = ' + str(taken) + ' seconds (' + str(taken/total*100) + ' % of total)\n')
-            f.write('Cutted out = ' + str(lost) + ' seconds (' + str(lost/total*100) + ' % of total)\n\n')
+    with open(model_folder + '/parametri.txt', 'w') as f:
+        f.write('Total = ' + str(total) + ' seconds\n')
+        f.write('Trained on = ' + str(taken) + ' seconds (' + str(taken/total*100) + ' % of total)\n')
+        f.write('Cutted out = ' + str(lost) + ' seconds (' + str(lost/total*100) + ' % of total)\n\n')
 
-        save_model_stats(X, y_AHA, y_MACS, y_pred, modello, model["params"]['n_clusters'], model_folder)
-
-    else:
-        print("The model has already been trained!")
-        modello = jl.load(model_folder + "/trained_model")
+    save_model_stats(X, y_AHA, y_MACS, y_pred, model, specs["params"]['n_clusters'], model_folder)
 
     if plot_clusters:
-        plot_cluster_algorithm(modello, X, modello.n_clusters)
+        plot_cluster_algorithm(model, X, model.n_clusters)
 
-    print("TRAINING COMPLETED (Model " + model_name + " ready)")
+    print("TRAINING COMPLETED (Model ready)")
