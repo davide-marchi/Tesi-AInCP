d041c45c2639a57dcc481b5716652c8c30f60fd7
Davide <davidemarchi00@gmail.com>
2023-02-26 20:13:59 +0100

    Modificato "rinnovo". Attualmente non funziona perché non riesce ad appendere ad uno dei due dataframe

---
+++ Rinnovo Val - Test/main_train_validate.py
@@ -0,0 +1,80 @@
+import json
+import os
+import hashlib
+import itertools
+import pandas as pd
+from sktime.base import BaseEstimator
+from train_best_model import train_best_model
+from save_AHA_stats import save_AHA_stats
+from save_week_stats import save_week_stats
+
+if os.getlogin() == 'david':
+    data_folder = 'C:/Users/david/Documents/University/Tesi/Python AInCP/only AC/'
+elif os.getlogin() == 'giord':
+    data_folder = 'C:/Users/giord/Downloads/only AC data/only AC/'
+else:
+    data_folder = input("Enter the data folder path: ")
+
+# Cambio la directory di esecuzione in quella dove si trova questo file
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+kmeans_type = 'sktime.clustering.k_means.TimeSeriesKMeans'
+kmeans_params =  {'averaging_method': ['mean'], 'init_algorithm': ['kmeans++', 'forgy'], 'metric': ['euclidean'], 'n_clusters': [2]}
+kmeans = (kmeans_type, kmeans_params)
+
+kmedoids_type = 'sktime.clustering.k_medoids.TimeSeriesKMedoids'
+kmedoids_params = {'distance_params': [None], 'init_algorithm': ['random'], 'max_iter': [300], 'metric': ['dtw'], 'n_clusters': [8], 'n_init': [10], 'random_state': [None], 'tol': [1e-06], 'verbose': [False]}
+kmedoids = (kmedoids_type, kmedoids_params)
+
+cnn_type = 'sktime.classification.deep_learning.cnn.CNNClassifier'
+cnn_params =  {'activation': ['sigmoid'], 'avg_pool_size': [3], 'batch_size': [16], 'callbacks': [None], 'kernel_size': [7], 'loss': ['mean_squared_error'], 'metrics': [None], 'n_conv_layers': [2], 'n_epochs': [2000], 'optimizer': [None], 'random_state': [None], 'use_bias': [True], 'verbose': [False]}
+cnn = (cnn_type, cnn_params)
+
+boss_type = 'sktime.classification.dictionary_based._boss.BOSSEnsemble'
+boss_params = {'alphabet_size': [4], 'feature_selection': ['none'], 'max_ensemble_size': [500], 'max_win_len_prop': [1], 'min_window': [10], 'n_jobs': [1], 'random_state': [None], 'save_train_predictions': [False], 'threshold': [0.92], 'typed_dict': [True], 'use_boss_distance': [True]}
+boss = (boss_type, boss_params)
+
+shapedtw_type = 'sktime.classification.distance_based._shape_dtw.ShapeDTW'
+shapedtw_params =  {'shape_descriptor_function': ['raw', 'paa'] }
+shapedtw = (shapedtw_type, shapedtw_params)
+
+l_method =              ['concat', 'ai']               # ['concat','difference', 'ai']
+l_window_size =         [600, 900]                               # [300, 600, 900]
+l_gridsearch_specs =    [kmeans]                                    # [kmeans, kmedoids, cnn, boss, shapedtw]
+
+estimators_df = pd.DataFrame()
+best_estimators_df = pd.DataFrame()
+
+for method, window_size, gridsearch_specs in itertools.product(l_method, l_window_size, l_gridsearch_specs):
+
+    print('Method: ', method, '\nWindow size: ', window_size, '\nGrid search specs: ', gridsearch_specs)
+
+    model_type, model_params = gridsearch_specs
+
+    gridsearch_folder = "Trained_models/" + method + "/" + str(window_size) + "_seconds/" + model_type.split(".")[-1] + "/" + "gridsearch_" + hashlib.sha256(json.dumps(model_params, sort_keys=True).encode()).hexdigest()[:10] + "/"
+
+    if not(os.path.exists(gridsearch_folder + "best_estimator.zip")) or not(os.path.exists(gridsearch_folder + 'GridSearchCV_stats/cv_results.csv')):
+
+        #print("Model not found -> Training started\n")
+        train_best_model(data_folder, gridsearch_folder, model_type, model_params, method, window_size)
+
+    cv_results = pd.read_csv(gridsearch_folder + 'GridSearchCV_stats/cv_results.csv')
+    cv_results['method'] = method
+    cv_results['window_size'] = window_size
+    cv_results['model_type'] = model_type
+
+    estimators_df = pd.concat([estimators_df, cv_results])
+    best_estimators_df = pd.concat([best_estimators_df, cv_results.iloc[cv_results['rank_test_score'].argmin()]])
+
+print(estimators_df.sort_values(by=['mean_test_score', 'std_test_score'], ascending=False))
+print('\n\n------------------------\n\n')
+print(best_estimators_df.sort_values(by=['mean_test_score', 'std_test_score'], ascending=False))
+
+estimators_df.sort_values(by=['mean_test_score', 'std_test_score'], ascending=False).to_csv('estimators_results.csv')
+best_estimators_df.sort_values(by=['mean_test_score', 'std_test_score'], ascending=False).to_csv('best_estimators_results.csv')
+
+# DEVE OUTPUTTARE
+#   - IL MODELLO MIGLIORE DI TUTTI E IL SUO SCORE
+#   - IL MODELLO SUPERVISED MIGLIORE E IL SUO SCORE
+#   - IL MODELLO UNSUPERVISED MIGLIORE DI TUTTI E IL SUO SCORE
+#   - (DATO CHE CI SIAMO CHE STAMPI UNA VERA E PROPRIA CLASSIFICA)
