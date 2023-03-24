4d58a99586800a795115ae5e4eb8cd5c198005f8
Davide <davidemarchi00@gmail.com>
2023-03-24 11:54:14 +0100

    Aggiunto file per la correlazione TESI_plot_corcoeff

--- Rinnovo Val - Test/main_test.py
+++ Rinnovo Val - Test/main_test.py
@@ -33,7 +33,9 @@ best_estimators_df = pd.read_csv('best_estimators_results.csv', index_col=0).sor
 #estimators_specs_list.append(best_estimators_df[best_estimators_df['method'] == 'ai'].iloc[0])
 #estimators_specs_list.append(best_estimators_df[best_estimators_df['method'] == 'difference'].iloc[0])
 #estimators_specs_list = [row for index, row in best_estimators_df[(best_estimators_df['mean_test_score'] >= 0.975) & (best_estimators_df['window_size'] == 600)].iterrows()]
-estimators_specs_list = [row for index, row in best_estimators_df[(best_estimators_df['mean_test_score'] >= 0.938) & (best_estimators_df['window_size'] == 300)].iterrows()]
+#estimators_specs_list = [row for index, row in best_estimators_df[(best_estimators_df['mean_test_score'] >= 0.938) & (best_estimators_df['window_size'] == 300)].iterrows()]
+
+estimators_specs_list = [row for index, row in best_estimators_df[(best_estimators_df['mean_test_score'] >= 0.954) & (best_estimators_df['window_size'] == 300)].iterrows()]
 print('Expected estimators: ',len(estimators_specs_list))
 estimators_list = []
 model_id_concat = ''
@@ -223,6 +225,4 @@ metadata.to_csv(stats_folder + '/predictions_dataframe.csv')
 #metadata.plot.scatter(x='healthy_percentage', y='AI_week', c='MACS', colormap='viridis').get_figure().savefig(stats_folder + 'plot_healthyPerc_AI_week.png')
 #metadata.plot.scatter(x='healthy_percentage', y='AI_aha', c='MACS', colormap='viridis').get_figure().savefig(stats_folder + 'plot_healthyPerc_AI_aha.png')
 
-
 #print("Coefficiente di Pearson tra hp e aha:          ", (np.corrcoef(metadata['healthy_percentage'], metadata['AHA'].values))[0][1])
-
